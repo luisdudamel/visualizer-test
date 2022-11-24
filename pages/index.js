fe305e739fb995003df8bc7f4cbca24eaf98b686
Luis Dudamel <ladusa@gmail.com>
2022-11-24 13:52:18 +0100

    Added favicon. Updated nextjs version to latest

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
+import Head from 'next/head'
+
 const Home = () => {
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
-      ESTUDIO CACTUS VISUALIZER TEST
-    </div>
+    <>
+      <Head>
+        <link rel='shortcut icon' href='/img/logo-estudio-cactus.svg' />
+      </Head>
+      <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+        ESTUDIO CACTUS VISUALIZER TEST
+      </div>
+    </>
   )
 }
 
