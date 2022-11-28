7d1bbde4764e41b0394c64db172ddc312ca38501
Luis Dudamel <ladusa@gmail.com>
2022-11-28 20:27:48 +0100

    Added useref reference to ul container for scrolling

--- src/components/MaterialSelector/MaterialSelector.js
+++ src/components/MaterialSelector/MaterialSelector.js
@@ -1,5 +1,6 @@
 import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useEffect, useRef, useState } from 'react'
 
 import MaterialSample from '../MaterialSample/MaterialSample'
 
@@ -10,12 +11,23 @@ const MaterialSelector = ({
   currentActiveLayers,
   currentActiveLayersData
 }) => {
+  const [scrollRef, setScrollRef] = useState()
+  const selectorRef = useRef(null)
   const setNewLayer = newLayerToSet => {
     const newLayerToAdd = materialPreviewList.materials.find(
       material => material.id === newLayerToSet
     )
 
     setLayers(newLayerToAdd)
+  }
+
+  useEffect(() => {
+    setScrollRef(selectorRef)
+  }, [])
+
+  const scroll = scrollOffset => {
+    scrollRef.current.scrollLeft += scrollOffset
+    console.log(scrollRef)
   }
 
   return (
@@ -34,6 +46,7 @@ const MaterialSelector = ({
       </div>
       <div className='xl:pl-[10px] justify-center top-[110%] h-[80px] xl:h-full w-full xl:w-[150px] items-center flex xl:flex-col h-full absolute xl:top-auto xl:left-[100%]'>
         <button
+          onClick={() => scroll(-80)}
           type='button'
           className='z-2 h-[30px] w-[100px] flex-2 text-start xl:text-end overflow-hidden text-[12px] focus-visible:overflow-visible'
         >
@@ -47,24 +60,25 @@ const MaterialSelector = ({
         </button>
 
         <ul
+          ref={selectorRef}
           className={
             materialPreviewList.materials.length > 2
-              ? 'py-10 xl:flex-1 w-[500px] xl:w-[200px] xl:overflow-visible flex xl:flex-col gap-[10px] no-scrollbar justify-between xl:justify-start sm:justify-center overflow-auto'
-              : 'py-10 xl:flex-1 w-[500px] xl:w-[200px] xl:overflow-visible flex xl:flex-col gap-[10px] no-scrollbar justify-center xl:justify-start overflow-auto'
+              ? 'py-10 xl:flex-1 w-[500px] xl:w-[200px] xl:overflow-visible flex xl:flex-col gap-[10px] no-scrollbar justify-between xl:justify-start sm:justify-center overflow-x-scroll'
+              : 'py-10 xl:flex-1 w-[500px] xl:w-[200px] xl:overflow-visible flex xl:flex-col gap-[10px] no-scrollbar justify-center xl:justify-start overflow-x-scroll'
           }
         >
           {materialPreviewList.materials.map(material => {
             return (
               <li
                 key={material.id}
-                className='justify-end w-[180px] sm:max-xl:w-auto text-neutral-600 flex h-[70px] flex-row '
+                className='justify-end lg:w-[180px] sm:max-xl:w-auto text-neutral-600 flex h-[70px] flex-row '
               >
                 {currentActiveLayers.includes(material.name) ? (
-                  <div className='relative px-2 transition-all duration-800 w-[80px] left-[3px] text-[10px] w-full flex justify-center items-center flex h-full bg-white rounded-l border-white'>
+                  <div className='relative px-2 transition-all duration-800 w-[100px] left-[3px] lg:w-[80px] text-[10px] w-full flex justify-center items-center flex h-full bg-white rounded-l border-white'>
                     {material.name}
                   </div>
                 ) : (
-                  <div className='hidden transition-all duration-800 xl:block relative px-2 w-[80px] left-[3px] text-[10px] w-full flex justify-center items-center flex h-full rounded-l border-white' />
+                  <div className='hidden transition-all duration-800 xl:block relative px-2 w-[80px] lg:w-[80px] left-[3px] text-[10px] w-full flex justify-center items-center flex h-full rounded-l border-white' />
                 )}
 
                 <MaterialSample
@@ -77,6 +91,7 @@ const MaterialSelector = ({
         </ul>
 
         <button
+          onClick={() => scroll(80)}
           type='button'
           className='z-2 h-[30px] flex-2 w-[100px] text-end xl:text-end overflow-hidden text-[12px] focus-visible:overflow-visible'
         >
