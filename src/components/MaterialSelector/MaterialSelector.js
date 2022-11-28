351d3c3f6717dda675f19fcb89afc8681d737426
Luis Dudamel <ladusa@gmail.com>
2022-11-28 11:53:08 +0100

    Added transition ease-in-out for materialPreview elements

--- src/components/MaterialSelector/MaterialSelector.js
+++ src/components/MaterialSelector/MaterialSelector.js
@@ -36,12 +36,14 @@ const MaterialSelector = ({
       <div className='xl:pl-[10px] justify-center top-[110%] h-[80px] xl:h-full w-full xl:w-auto items-center flex xl:flex-col h-full absolute xl:top-auto xl:left-[100%]'>
         <button
           type='button'
-          className='z-2 h-[30px] w-[30px] overflow-hidden text-[12px] focus-visible:overflow-visible'
+          className='z-2 h-[30px] w-[100%] text-end overflow-hidden text-[12px] focus-visible:overflow-visible'
         >
-          <FontAwesomeIcon
-            className='text-white text-center text-2xl xl:rotate-0 -rotate-90'
-            icon={faChevronUp}
-          />
+          <div>
+            <FontAwesomeIcon
+              className='text-white text-center text-2xl xl:rotate-0 -rotate-90'
+              icon={faChevronUp}
+            />
+          </div>
           Go up
         </button>
         <ul className='py-10 xl:flex-1 flex xl:flex-col gap-[10px]'>
@@ -52,11 +54,11 @@ const MaterialSelector = ({
                 className='justify-end text-neutral-600 flex h-[70px] flex-row '
               >
                 {currentActiveLayers.includes(material.name) ? (
-                  <div className='relative px-2 w-[80px] left-[3px] text-[10px] w-full flex justify-center items-center flex h-full bg-white rounded-l border-white'>
+                  <div className='relative px-2 transition-all duration-800 w-[80px] left-[3px] text-[10px] w-full flex justify-center items-center flex h-full bg-white rounded-l border-white'>
                     {material.name}
                   </div>
                 ) : (
-                  <div className='relative px-2 w-[80px] left-[3px] text-[10px] w-full flex justify-center items-center flex h-full rounded-l border-white'></div>
+                  <div className='hidden transition-all duration-800 xl:block relative px-2 w-[80px] left-[3px] text-[10px] w-full flex justify-center items-center flex h-full rounded-l border-white'></div>
                 )}
 
                 <MaterialSample
@@ -69,12 +71,14 @@ const MaterialSelector = ({
         </ul>
         <button
           type='button'
-          className='z-2 h-[30px] w-[30px] overflow-hidden text-[12px] focus-visible:overflow-visible'
+          className='z-2 h-[30px] w-[100%] text-end overflow-hidden text-[12px] focus-visible:overflow-visible'
         >
-          <FontAwesomeIcon
-            className='text-white text-center text-2xl xl:rotate-0 -rotate-90'
-            icon={faChevronDown}
-          />
+          <div>
+            <FontAwesomeIcon
+              className='text-white text-center text-2xl xl:rotate-0 -rotate-90'
+              icon={faChevronDown}
+            />
+          </div>
           Go down
         </button>
       </div>
