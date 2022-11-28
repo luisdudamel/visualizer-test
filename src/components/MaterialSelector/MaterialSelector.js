51392b04740555302537160829ee5e52f14569ef
Luis Dudamel <ladusa@gmail.com>
2022-11-28 18:58:16 +0100

    Added justify-center for mobile and justify-start for desktop

--- src/components/MaterialSelector/MaterialSelector.js
+++ src/components/MaterialSelector/MaterialSelector.js
@@ -32,7 +32,7 @@ const MaterialSelector = ({
           Close material selector
         </button>
       </div>
-      <div className='xl:pl-[10px] justify-center top-[110%] h-[80px] xl:h-full w-full xl:w-auto items-center flex xl:flex-col h-full absolute xl:top-auto xl:left-[100%]'>
+      <div className='xl:pl-[10px] justify-center top-[110%] h-[80px] xl:h-full w-full xl:w-[150px] items-center flex xl:flex-col h-full absolute xl:top-auto xl:left-[100%]'>
         <button
           type='button'
           className='z-2 h-[30px] w-[100px] flex-2 text-start xl:text-end overflow-hidden text-[12px] focus-visible:overflow-visible'
@@ -49,15 +49,15 @@ const MaterialSelector = ({
         <ul
           className={
             materialPreviewList.materials.length > 2
-              ? 'py-10 xl:flex-1 w-[500px] flex xl:flex-col gap-[10px] no-scrollbar justify-between sm:justify-center overflow-auto'
-              : 'py-10 xl:flex-1 w-[500px] flex xl:flex-col gap-[10px] no-scrollbar justify-center overflow-auto'
+              ? 'py-10 xl:flex-1 w-[500px] xl:w-[200px] xl:overflow-visible flex xl:flex-col gap-[10px] no-scrollbar justify-between xl:justify-start sm:justify-center overflow-auto'
+              : 'py-10 xl:flex-1 w-[500px] xl:w-[200px] xl:overflow-visible flex xl:flex-col gap-[10px] no-scrollbar justify-center xl:justify-start overflow-auto'
           }
         >
           {materialPreviewList.materials.map(material => {
             return (
               <li
                 key={material.id}
-                className='justify-end text-neutral-600 flex h-[70px] flex-row '
+                className='justify-end w-[180px] sm:max-xl:w-auto text-neutral-600 flex h-[70px] flex-row '
               >
                 {currentActiveLayers.includes(material.name) ? (
                   <div className='relative px-2 transition-all duration-800 w-[80px] left-[3px] text-[10px] w-full flex justify-center items-center flex h-full bg-white rounded-l border-white'>
