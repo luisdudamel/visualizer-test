d7f8d244e3e84f97a0037f1c0f567f2648436344
Luis Dudamel <ladusa@gmail.com>
2022-11-28 01:06:35 +0100

    Added test for click on material sample

--- components/MaterialSample/MaterialSample.test.js
+++ components/MaterialSample/MaterialSample.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import { mockMaterialPreview } from '../../mocks/mockMaterials'
 import MaterialSample from './MaterialSample'
+import userEvent from '@testing-library/user-event'
 
 describe('Given a MaterialSample function', () => {
   describe("When it's invoked with a material named `Gold Wood`", () => {
@@ -13,6 +14,29 @@ describe('Given a MaterialSample function', () => {
       })
 
       expect(selectMaterialButton).toBeInTheDocument()
+    })
+  })
+
+  describe("When it's invoked with a material named `Gold Wood` and a function", () => {
+    describe('And the user clicks on the button with the text `Gold Wood material sample Select Gold Wood material`', () => {
+      test('Then it should call the received function`', async () => {
+        const expectedButtonText =
+          'Gold Wood material sample Select Gold Wood material'
+        const mockFunction = jest.fn()
+
+        render(
+          <MaterialSample
+            setLayerFunction={mockFunction}
+            material={mockMaterialPreview}
+          />
+        )
+        const selectMaterialButton = screen.getByRole('button', {
+          name: expectedButtonText
+        })
+        await userEvent.click(selectMaterialButton)
+
+        expect(mockFunction).toHaveBeenCalled()
+      })
     })
   })
 })
