f28aadfaff91a50c2db0951d13eefd0b1417919b
Luis Dudamel <ladusa@gmail.com>
2022-11-28 11:30:36 +0100

    Modified material map for mapping over array of material strings

--- src/components/MaterialSelector/MaterialSelector.js
+++ src/components/MaterialSelector/MaterialSelector.js
@@ -51,7 +51,7 @@ const MaterialSelector = ({
                 key={material.id}
                 className='justify-end text-neutral-600 flex h-[70px] flex-row '
               >
-                {currentActiveLayersData.includes(material.name) && (
+                {currentActiveLayers.includes(material.name) && (
                   <div className='relative px-2 w-[80px] left-[3px] text-[10px] w-full flex justify-center items-center flex h-full bg-white rounded-l border-white'>
                     {material.name}
                   </div>
