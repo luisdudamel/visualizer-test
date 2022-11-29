f82574d6d573b19eaa4812bb3a10504612e9615f
Luis Dudamel <ladusa@gmail.com>
2022-11-29 02:31:36 +0100

    Removed padding-right from fontawesomeicon on lg breakpoint

--- src/components/MaterialSelector/MaterialSelector.js
+++ src/components/MaterialSelector/MaterialSelector.js
@@ -43,41 +43,41 @@ const MaterialSelector = ({
           Close material selector
         </button>
       </div>
-      <div className='xl:pl-[10px] justify-center top-[110%] h-[80px] xl:h-full w-full xl:w-[150px] items-center flex xl:flex-col h-full absolute xl:top-auto xl:left-[100%]'>
+      <div className='xl:ml-[10px] lg:items-end justify-center top-[110%] h-[80px] xl:h-full w-full xl:w-[150px] items-center flex xl:flex-col h-full absolute xl:top-auto xl:left-[100%]'>
         <button
           onClick={() => scroll(-80)}
           type='button'
           className='z-2 h-[30px] w-[100px] flex-2 text-start xl:text-end overflow-hidden text-[12px] focus-visible:overflow-visible'
         >
-          <div className='pl-[15px] xl:pr-[15px]'>
+          <div className='pl-[15px] lg:pr-[10px]'>
             <FontAwesomeIcon
               className='text-white text-center text-2xl xl:rotate-0 -rotate-90'
               icon={faChevronUp}
             />
           </div>
-          Go up
+          Previous
         </button>
 
         <ul
           ref={selectorRef}
           className={
             materialPreviewList.materials.length > 2
-              ? 'py-10 xl:flex-1 w-[500px] xl:w-[200px] xl:overflow-visible flex xl:flex-col gap-[10px] no-scrollbar scroll-smooth justify-between xl:justify-start sm:justify-center overflow-x-scroll'
-              : 'py-10 xl:flex-1 w-[500px] xl:w-[200px] xl:overflow-visible flex xl:flex-col gap-[10px] no-scrollbar scroll-smooth justify-center xl:justify-start overflow-x-scroll'
+              ? 'py-10 xl:flex-1 w-[500px] xl:w-[150px] xl:overflow-visible flex xl:flex-col gap-[10px] no-scrollbar scroll-smooth justify-between xl:justify-start sm:justify-center overflow-x-scroll'
+              : 'py-10 xl:flex-1 w-[500px] xl:w-[150px] xl:overflow-visible flex xl:flex-col gap-[10px] no-scrollbar scroll-smooth justify-center xl:justify-start overflow-x-scroll'
           }
         >
           {materialPreviewList.materials.map(material => {
             return (
               <li
                 key={material.id}
-                className='drop-shadow justify-end lg:w-[180px] sm:max-xl:w-auto text-neutral-600 flex h-[70px] flex-row '
+                className='drop-shadow justify-end lg:w-[150px] sm:max-xl:w-auto text-neutral-600 flex h-[70px] flex-row '
               >
                 {currentActiveLayers.includes(material.name) ? (
-                  <div className='relative px-2 transition-all duration-100 w-[100px] left-[3px] lg:w-[80px] text-[10px] w-full flex justify-center items-center flex h-full bg-white rounded-l border-white'>
+                  <div className='relative transition-all duration-100 w-[100px] left-[3px] lg:w-[80px] text-[9px] w-full flex justify-center items-center flex h-full bg-white rounded-l border-white'>
                     {material.name}
                   </div>
                 ) : (
-                  <div className='hidden transition-all duration-100 xl:block relative px-2 w-[80px] lg:w-[80px] left-[3px] text-[10px] w-full flex justify-center items-center flex h-full rounded-l border-white' />
+                  <div className='hidden transition-all duration-100 xl:block relative px-2 w-[80px] lg:w-[80px] left-[3px] text-[9px] w-full flex justify-center items-center flex h-full rounded-l border-white' />
                 )}
 
                 <MaterialSample
@@ -92,15 +92,15 @@ const MaterialSelector = ({
         <button
           onClick={() => scroll(80)}
           type='button'
-          className='z-2 h-[30px] flex-2 w-[100px] text-end xl:text-end overflow-hidden text-[12px] focus-visible:overflow-visible'
+          className='z-2 h-[30px] flex-2 w-[100px] text-end overflow-hidden text-[12px] focus-visible:overflow-visible'
         >
-          <div className='pr-[15px]'>
+          <div className='pr-[15px] lg:pr-[10px]'>
             <FontAwesomeIcon
               className='text-white text-center text-2xl xl:rotate-0 -rotate-90'
               icon={faChevronDown}
             />
           </div>
-          Go down
+          Next
         </button>
       </div>
     </>
